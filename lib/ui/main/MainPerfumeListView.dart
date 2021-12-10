29f3352885e4c21613e672ac3fdeb6a6d8b4fd31
Haeseong Jeon <seong0428@gmail.com>
2021-12-10 23:39:18 +0900

    feat: 향수 상세 화면 appbar 수정

--- lib/ui/main/MainPerfumeListView.dart
+++ lib/ui/main/MainPerfumeListView.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/// brand 선택해서 오는 경우,
+/// 향수 선택해서 오는 경우
 class MainPerfumeListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/ui/perfume/PerfumeDetailView.dart
+++ lib/ui/perfume/PerfumeDetailView.dart
@@ -5,8 +5,158 @@ class PerfumeDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text('perfume detail view'),
+      appBar: AppBar(
+        title: Text(
+          'Philosykos Eau de Toiletteaa aaaa',
+          style: TextStyle(
+            color: Color(0xff1b1b1b),
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        // foregroundColor: Color(0xff1b1b1b),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Color(0xff1b1b1b),
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          // 향수 이미지
+          Container(
+            height: 375,
+            child: Image.network(
+                'https://fimgs.net/mdimg/perfume/375x500.69123.jpg'),
+          ),
+          // 향수 이름, 브랜드 이름
+          Container(
+            height: 120,
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Text('DIPTYQUE'),
+                    Text('Philosykos Eau de Toiletteaa aaaa'),
+                    Text('3명이 이 향수에 관심있어요.')
+                  ],
+                ),
+                Text('디깅 아이콘'),
+              ],
+            ),
+          ),
+          // 탭
+          Container(
+            height: 48,
+            child: Row(
+              children: [
+                Text('상품 정보'),
+                Text('리뷰'),
+              ],
+            ),
+          ),
+          // accord
+          Container(
+            height: 471,
+            child: Column(
+              children: [
+                Text('구성 성분'),
+                Text('Vanila'),
+                Text('Powdery'),
+                Text('Violet'),
+                Text('Earthy'),
+                Text('Sweet'),
+                Text('Aromatic'),
+                Text('Green'),
+                Text('Floral'),
+                Text('Woody'),
+              ],
+            ),
+          ),
+          // note
+          Container(
+            height: 375,
+            child: Column(
+              children: [
+                Text('향수 노트'),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child:
+                          ElevatedButton(onPressed: () {}, child: Text('Top')),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: Text('Middle')),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          onPressed: () {}, child: Text('Bottom')),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 200,
+                  child: GridView.count(
+                    childAspectRatio: 100 / 45,
+                    crossAxisCount: 3,
+                    children: [
+                      Text('Note 1'),
+                      Text('Note 2'),
+                      Text('Note 3'),
+                      Text('Note 4'),
+                      Text('Note 5'),
+                      Text('더 보기'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // 유사한 향수
+          Container(
+            height: 375,
+            child: Column(
+              children: [
+                Text('이 향수와 비슷한 제품'),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('이미지 1'),
+                        Text('브랜드 이름 1'),
+                        Text('향수 이름 1'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text('이미지 2'),
+                        Text('브랜드 이름 2'),
+                        Text('향수 이름 2'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text('이미지 3'),
+                        Text('브랜드 이름 3'),
+                        Text('향수 이름 3'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
