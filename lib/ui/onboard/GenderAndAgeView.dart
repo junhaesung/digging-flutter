b8067411f9be9ed1aebdf03aadde7c6867020ffd
Haeseong Jeon <seong0428@gmail.com>
2021-12-14 02:07:57 +0900

    feat: 성별 선택 레이아웃

--- lib/ui/onboard/GenderAndAgeView.dart
+++ lib/ui/onboard/GenderAndAgeView.dart
@@ -8,7 +8,8 @@ class GenderAndAgeView extends StatefulWidget {
 }
 
 class _GenderAndAgeView extends State<GenderAndAgeView> {
-  var _selectedNoteGroups = 0;
+  // TODO: 성별 선택 레이아웃 작성중
+  var _selectedGender = 0;
   var _selectedAgeGroup = 0;
 
   @override
@@ -22,111 +23,9 @@ class _GenderAndAgeView extends State<GenderAndAgeView> {
             padding: const EdgeInsets.symmetric(horizontal: 10.0),
             child: ListView(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 32,
-                    left: 10,
-                    right: 10,
-                    bottom: 44,
-                  ),
-                  child: Container(
-                    child: Text(
-                      '성별과 \n나이를 선택해주세요.',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff1d1d20),
-                      ),
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '성별',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff1d1d20),
-                      ),
-                    ),
-                    Container(
-                      height: 200,
-                      child: GridView.count(
-                        crossAxisCount: 2,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                      'images/onboarding/selector_gender.png'),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '여성',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Color(0xff888888),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                      'images/onboarding/selector_gender.png'),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  '남성',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Color(0xff888888),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '나이',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff1d1d20),
-                      ),
-                    ),
-                    Container(height: 28),
-                    Container(
-                      height: 100,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 6.0),
-                        child: GridView.count(
-                          crossAxisSpacing: 8.0,
-                          childAspectRatio: 1,
-                          crossAxisCount: 5,
-                          children: _getAgeGroupWidgets(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                _titleWidget(),
+                _genderWidget(),
+                _ageGroupWidget(),
               ],
             ),
           ),
@@ -210,6 +109,123 @@ class _GenderAndAgeView extends State<GenderAndAgeView> {
               fontSize: 14,
               color: Color(0xff888888),
               decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _genderButtonWidget(String genderText) {
+    var selected = false;
+    if (_selectedGender == 1 && genderText == '여성') {
+      selected = true;
+    }
+    if (_selectedGender == 2 && genderText == '남성') {
+      selected = true;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            var current = genderText == '여성' ? 1 : 2;
+            _selectedGender = _selectedGender != current ? current : 0;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(selected
+                  ? 'images/onboarding/gender_selected.png'
+                  : 'images/onboarding/gender_normal.png'),
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              genderText,
+              style: TextStyle(
+                fontSize: 16,
+                color: Color(selected ? 0xff1b1b1b : 0xff888888),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 32,
+        left: 10,
+        right: 10,
+        bottom: 44,
+      ),
+      child: Container(
+        child: Text(
+          '성별과 \n나이를 선택해주세요.',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff1d1d20),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _genderWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '성별',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff1d1d20),
+          ),
+        ),
+        Container(
+          height: 200,
+          child: GridView.count(
+            crossAxisCount: 2,
+            children: [
+              _genderButtonWidget('여성'),
+              _genderButtonWidget('남성'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _ageGroupWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '나이',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xff1d1d20),
+          ),
+        ),
+        Container(height: 28),
+        Container(
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6.0),
+            child: GridView.count(
+              crossAxisSpacing: 8.0,
+              childAspectRatio: 1,
+              crossAxisCount: 5,
+              children: _getAgeGroupWidgets(),
             ),
           ),
         ),
