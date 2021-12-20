65b6565af5244f826392ea780f1a3378f42c3c9f
Haeseong Jeon <seong0428@gmail.com>
2021-12-20 23:00:52 +0900

    feat: api 요청시 storage 에서 토큰 읽도록 변경

--- lib/adapter/api/DiggingApi.dart
+++ lib/adapter/api/DiggingApi.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:digging/adapter/api/model/LoginRequest.dart';
 import 'package:digging/adapter/api/model/PerfumeSimple.dart';
+import 'package:digging/adapter/storage/secure_storage_api.dart';
 import 'package:http/http.dart' as http;
 
 import 'model/ApiResponse.dart';
@@ -11,9 +12,8 @@ import 'model/NoteSimple.dart';
 import 'model/PerfumeDetail.dart';
 
 class DiggingApi {
-  String _host = "api.digging.me";
-  String _accessToken =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhY20tYXBpLWRldmVsb3AiLCJtZW1iZXJJZCI6MjExNzd9.sQta6Xokjdoc7Bt_KaLVYxMX8nBhqDdJvbqRX5eErms';
+  final TokenStorage _tokenStorage = TokenStorage();
+  final String _host = "api.digging.me";
 
   Future<LoginResponse> login(String uuid) async {
     final loginRequest = LoginRequest(
@@ -32,42 +32,50 @@ class DiggingApi {
   }
 
   Future<BrandDetail> fetchBrand(int brandId) async {
+    final accessToken = await _getAccessToken();
     return http
         .get(
           Uri.http(_host, '/api/v1/brands/$brandId'),
-          headers: {'Authorization': 'Bearer $_accessToken'},
+          headers: {'Authorization': 'Bearer $accessToken'},
         )
         .then((value) => ApiResponse.brandData(json.decode(value.body)))
         .then((value) => value.data!.brandDetail);
   }
 
   Future<PerfumeDetail> fetchPerfume(int perfumeId) async {
+    final accessToken = await _getAccessToken();
     return http
         .get(
           Uri.http(_host, '/api/v1/perfumes/$perfumeId'),
-          headers: {'Authorization': 'Bearer $_accessToken'},
+          headers: {'Authorization': 'Bearer $accessToken'},
         )
         .then((value) => ApiResponse.perfumeData(json.decode(value.body)))
         .then((value) => value.data!.perfumeDetail);
   }
 
-  Future<List<PerfumeSimple>> fetchPerfumes(int noteId) {
+  Future<List<PerfumeSimple>> fetchPerfumes(int noteId) async {
+    final accessToken = await _getAccessToken();
     return http
         .get(
           Uri.http(_host, '/api/v1/perfumes', {'noteId': noteId.toString()}),
-          headers: {'Authorization': 'Bearer $_accessToken'},
+          headers: {'Authorization': 'Bearer $accessToken'},
         )
         .then((value) => ApiResponse.perfumesData(json.decode(value.body)))
         .then((value) => value.data!.perfumeSimples);
   }
 
-  Future<List<NoteSimple>> fetchNotes(int noteGroupId) {
+  Future<List<NoteSimple>> fetchNotes(int noteGroupId) async {
+    final accessToken = await _getAccessToken();
     return http
         .get(
           Uri.http(_host, '/api/v1/note-groups/$noteGroupId/notes'),
-          headers: {'Authorization': 'Bearer $_accessToken'},
+          headers: {'Authorization': 'Bearer $accessToken'},
         )
         .then((value) => ApiResponse.notesData(json.decode(value.body)))
         .then((value) => value.data!.noteSimples);
+  }
+
+  Future<String?> _getAccessToken() async {
+    return _tokenStorage.read();
   }
 }
