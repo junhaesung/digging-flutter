b61f7e62d0af2ea2fca2c37780978c82d1674898
Haeseong Jeon <seong0428@gmail.com>
2022-01-02 22:46:57 +0900

    feat: bottom nav bar 아이콘, 색상 적용

--- lib/ui/tab_page/bottom_tab_bar.dart
+++ lib/ui/tab_page/bottom_tab_bar.dart
@@ -1,3 +1,4 @@
+import 'package:digging/ui/design/digging_color.dart';
 import 'package:digging/ui/tab_page/tab_page_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,30 +8,43 @@ class BottomTabBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TabPageCubit, TabPageState>(
-      builder: (context, state) => BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: state.index,
-        onTap: (index) {
-          if (index == 0) {
-            context.read<TabPageCubit>().showMainPage();
-          }
-          if (index == 1) {
-            context.read<TabPageCubit>().showSearchPage();
-          }
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'search',
-          ),
-          // TODO: digging view
-        ],
-      ),
+      builder: (context, state) {
+        return BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: state.index,
+          onTap: (index) {
+            if (index == 0) {
+              context.read<TabPageCubit>().showMainPage();
+            }
+            if (index == 1) {
+              context.read<TabPageCubit>().showSearchPage();
+            }
+          },
+          selectedItemColor: DiggingColor.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(state.index == 0
+                    ? 'images/icon/bottom_home_selected.png'
+                    : 'images/icon/bottom_home.png',),
+                size: 20,
+              ),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(state.index == 1
+                    ? 'images/icon/bottom_search_selected.png'
+                    : 'images/icon/bottom_search.png'),
+                size: 20,
+              ),
+              label: 'search',
+            ),
+            // TODO: digging view
+          ],
+        );
+      },
     );
   }
 }
